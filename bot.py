5888d67912a591da8e98b087954e21f7abca2319
JBIsaik <41596157+JBIsaik@users.noreply.github.com>
2018-07-27 12:25:27 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,33 +4,28 @@ from discord.ext.commands import Bot
 import random
 
 Client = discord.Client()
-bot = commands.Bot(command_prefix="$")
-lines = open(r'spotify.txt').read().splitlines()
+bot = commands.Bot(command_prefix="<")
+lines = open(r'usermcalts.txt').read().splitlines()
 
 @bot.event
 async def on_ready():
     print('The bot is online!')
-    await bot.change_presence(game=discord.Game(name='Type <spotify'))
    
     
 @bot.command(pass_context=True)
-async def getalt spotify(ctx):
+async def minecraft(ctx):
     userName = ctx.message.author.name
     userID = ctx.message.author.id
 
-    if ctx.message.server:
+    if ctx.message.server
         myline = random.choice(lines)
         split = myline.partition(":")
-       
-        embed=discord.Embed(title="Spotify Account", color=0xf45eff)
+        
+        embed=discord.Embed(title="Minecraft Account", color=0xf45eff)
         embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
         embed.add_field(name="Email:", value=split[0], inline=False)
         embed.add_field(name="Password:", value=split[2], inline=False)
         await bot.send_message(ctx.message.author, embed=embed)
 
-        print("{} Typed $getalt spotify".format(userName))
-
-        client = discord.Client()
-
-    
-bot.run("NDcxOTUxOTAzNDEzMjM5ODI5.DjsTFA.602mzvyAxSkgRBn_p45t_X_DLbw")
+        print("{} Typed <minecraft".format(userName))
+bot.run("NDcyMzQyOTIzMzU5NDIwNDE2.Djx_Mg.mxPJVnByXsr69854PnIW0ZBf2_o")
