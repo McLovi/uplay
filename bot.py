94c9767ee1c4471114aa5b247152f903f91a7ffd
JBIsaik <41596157+JBIsaik@users.noreply.github.com>
2018-07-27 12:48:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ lines = open(r'spotify.txt').read().splitlines()
 @bot.event
 async def on_ready():
     print('The bot is online!')
-    await bot.change_presence(game=discord.Game(name='Type <spotify'))
+    await bot.change_presence(game=discord.Game(name='Type <uplay'))
    
     
 @bot.command(pass_context=True)
@@ -22,8 +22,8 @@ async def spotify(ctx):
         myline = random.choice(lines)
         split = myline.partition(":")
        
-        embed=discord.Embed(title="Spotify Account", color=0xf45eff)
-        embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
+        embed=discord.Embed(title="Uplay Account", color=0xf45eff)
+        embed.set_thumbnail(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ9lEkpFJyS4zJJtyvgqmTR7NoR4-Wg3BaOSmQbDZInmOtxvz6MA.png")
         embed.add_field(name="Email:", value=split[0], inline=False)
         embed.add_field(name="Password:", value=split[2], inline=False)
         await bot.send_message(ctx.message.author, embed=embed)
