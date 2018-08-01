802bc53f57e9d09b12b6b8fabc4c5947ea82344c
JBIsaik <41596157+JBIsaik@users.noreply.github.com>
2018-08-01 15:53:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,30 +4,33 @@ from discord.ext.commands import Bot
 import random
 
 Client = discord.Client()
-bot = commands.Bot(command_prefix="!")
-lines = open(r'uplay.txt').read().splitlines()
+bot = commands.Bot(command_prefix="$")
+lines = open(r'usermcalts.txt').read().splitlines()
 
 @bot.event
 async def on_ready():
-    print("The bot is online!")
-    await bot.change_presence(game=discord.Game(name='Type $spotify'))
-
-    @bot.command(pass_context=True)
-    async def gen(ctx):
-      userName = ctx.message.author.name
-      userID = ctx.message.author.id
+    print('The bot is online!')
+    await bot.change_presence(game=discord.Game(name='Type $uplay'))
+   
+@commands.cooldown(1, 10, commands.BucketType.user)
+@bot.command(pass_context=True)
+async def uplay(ctx):
+    userName = ctx.message.author.name
+    userID = ctx.message.author.id
 
     if ctx.message.server:
-        await bot.delete_message(ctx.message)
-    myline = random.choice(lines)
-    split = myline.partition(":")
-    
-    embed=discord.Embed(title="Minecraft Account", color=0xf45eff)
-    embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
-    embed.add_field(name="Email:", value=split[0], inline=False)
-    embed.add_field(name="Password:", value=split[2], inline=False)
-    await bot.send_message(ctx.message.author, embed=embed)
+        myline = random.choice(lines)
+        split = myline.partition(":")
+       
+        embed=discord.Embed(title="Uplay Account", color=0xf45eff)
+        embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
+        embed.add_field(name="Email:", value=split[0], inline=False)
+        embed.add_field(name="Password:", value=split[2], inline=False)
+        await bot.send_message(ctx.message.author, embed=embed)
 
-    print("{} Typed !gen".format(userName))
+        print("{} Typed $uplay".format(userName))
 
-bot.run("_fmGTbhVdQNOOom-Sg0UprmB88VCE5jd")
+        client = discord.Client()
+
+bot.run("NDcyMzU0NjA0ODMxNzM1ODE4.DkNMZw.hdafAMmzR-ir6lXdAW9FvXHixyE")
+
